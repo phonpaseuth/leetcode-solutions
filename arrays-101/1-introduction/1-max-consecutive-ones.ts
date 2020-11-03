7a6958fc2f0ec31561d61098bea253966749cbd1
Phonpaseuth Khamsenesouk <phonpaseuth@gmail.com>
2020-11-02 22:31:54 -0600

    Add "inserting items into an array" solutions

--- arrays-101/1-introduction/1-max-consecutive-ones.ts
+++ arrays-101/1-introduction/1-max-consecutive-ones.ts
@@ -1,4 +1,4 @@
-/*
+/**
 Given a binary array, find the maximum number of consecutive 1s in this array.
 
 Example 1:
--- arrays-101/1-introduction/3-squared-of-a-sorted-array.ts
+++ arrays-101/1-introduction/3-squared-of-a-sorted-array.ts
@@ -1,4 +1,4 @@
-/*
+/**
   Given an array of integers A sorted in non-decreasing order, return an array of the squares of each number, also in sorted non-decreasing order.
 
   
